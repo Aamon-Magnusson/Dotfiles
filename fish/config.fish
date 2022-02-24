8e791e5ca1a87bd3f5b67ff7bb63d6313618c1f3
Aamon Magnusson <aamonm6@gmail.com>
2022-02-24 13:03:48 -0700

    Added to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,9 +5,23 @@ end
 function fish_greeting
 end
 
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
+
 alias pacman="sudo pacman"
 alias ..="cd .."
 alias grep="grep --color=auto"
 alias ran="ranger"
+
+# git aliases
+alias gc="git commit -a"
+alias gp="git push"
+alias gpl="git pull"
+alias gd="git diff"
 
 starship init fish | source
