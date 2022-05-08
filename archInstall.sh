c8ef56a3b52d3fde5751d241c73bba6057996c34
Aamon Magnusson <aamonm6@gmail.com>
2022-05-07 20:31:04 -0600

    arch install script

---
+++ archInstall.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Make $HOME folder structure
+mkdir -p $HOME/.config
+mkdir -p $HOME/Programming
+mkdir -p $HOME/Desktop
+mkdir -p $HOME/Music
+mkdir -p $HOME/Public
+mkdir -p $HOME/Templates
+mkdir -p $HOME/Documents
+mkdir -p $HOME/Pictures
+mkdir -p $HOME/Programming
+mkdir -p $HOME/RandomPrograms
+mkdir -p $HOME/Screenshots
+mkdir -p $HOME/Downloads
+mkdir -p $HOME/Videos
+
+# Set up yay
+sudo pacman -S git
+cd $HOME/Programming
+git clone https://aur.archlinux.org/yay
+cd yay
+makepkg -si
+
