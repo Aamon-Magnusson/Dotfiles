871b13574070273305c8d403b5c2068f4ff72d22
Aamon Magnusson <aamonm6@gmail.com>
2024-05-28 21:53:34 -0600

    adding less and man to intall

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ $(which pacman) ];then
-	sudo pacman -S gvim alacritty fish ranger lsd neovim fd fzf npm ccls pandoc lua-language-server texlive zathura zathura-pdf-mupdf sxiv libreoffice-still plantuml github-cli
+	sudo pacman -S gvim alacritty fish ranger lsd neovim fd fzf npm ccls pandoc lua-language-server texlive zathura zathura-pdf-mupdf sxiv libreoffice-still plantuml github-cli man-db less
 fi
 
 ln -s /run/media/$USER ~/Drives
